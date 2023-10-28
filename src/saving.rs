3d77a8a790b38a6e64b6bcb14e16b069e03e989f
Wleter <marcraftzastepczy@gmail.com>
2023-10-28 21:57:17 +0200

    add saving complex numbers

--- src/saving.rs
+++ src/saving.rs
@@ -1,5 +1,7 @@
 use std::{fs::File, io::Write};
 
+use num::complex::Complex64;
+
 pub fn save_vec(filename: &str, data: Vec<Vec<f64>>, header: Vec<&str>) -> Result<(), std::io::Error> {
     let path = std::env::current_dir().unwrap();
     let path = path.to_str().unwrap();
@@ -20,10 +22,23 @@ pub fn save_vec(filename: &str, data: Vec<Vec<f64>>, header: Vec<&str>) -> Resul
     Ok(())
 }
 
-pub fn save_parameter_change(filename: &str, parameter: Vec<f64>, data: Vec<Vec<f64>>, header: Vec<&str>) -> Result<(), std::io::Error> {
+pub fn save_param_change(filename: &str, parameter: Vec<f64>, data: Vec<Vec<f64>>, header: Vec<&str>) -> Result<(), std::io::Error> {
     let combined_data = parameter.iter()
         .zip(data.iter())
         .map(|(p, d)| [vec![*p], d.to_owned()].concat())
+        .collect();
+
+    save_vec(filename, combined_data, header)
+}
+
+pub fn save_param_change_complex(filename: &str, parameter: Vec<f64>, data: Vec<Complex64>, header: Vec<&str>) -> Result<(), std::io::Error> {
+    let real_part = format!("{} re", header[1]);
+    let im_part = format!("{} im", header[1]);
+    let header = vec![header[0], &real_part, &im_part];
+    
+    let combined_data = parameter.iter()
+        .zip(data.iter())
+        .map(|(p, d)| vec![*p, d.re, d.im])
         .collect();
 
     save_vec(filename, combined_data, header)
