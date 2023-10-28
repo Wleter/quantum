12654e767d85154bb4588355805f8d750443f313
Wleter <marcraftzastepczy@gmail.com>
2023-10-28 14:00:58 +0200

    add saving parameter change

--- src/saving.rs
+++ src/saving.rs
@@ -1,14 +1,13 @@
 use std::{fs::File, io::Write};
 
-
-pub fn save_vec(filename: &str, data: Vec<Vec<f64>>, header: Vec<&'static str>) -> Result<(), std::io::Error> {
+pub fn save_vec(filename: &str, data: Vec<Vec<f64>>, header: Vec<&str>) -> Result<(), std::io::Error> {
     let path = std::env::current_dir().unwrap();
     let path = path.to_str().unwrap();
 
     let mut buf = header.join("\t");
     for value in data {
         let line= value.iter()
-            .map(ToString::to_string)
+            .map(|x| format!("{:e}", x))
             .collect::<Vec<String>>()
             .join("\t");
 
@@ -19,4 +18,13 @@ pub fn save_vec(filename: &str, data: Vec<Vec<f64>>, header: Vec<&'static str>) 
     file.write_all(buf.as_bytes())?;
 
     Ok(())
+}
+
+pub fn save_parameter_change(filename: &str, parameter: Vec<f64>, data: Vec<Vec<f64>>, header: Vec<&str>) -> Result<(), std::io::Error> {
+    let combined_data = parameter.iter()
+        .zip(data.iter())
+        .map(|(p, d)| [vec![*p], d.to_owned()].concat())
+        .collect();
+
+    save_vec(filename, combined_data, header)
 }
