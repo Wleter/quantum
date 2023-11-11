36023c2df5f578dc713b4f93d2f9b007a9c9ca44
Wleter <marcraftzastepczy@gmail.com>
2023-11-11 10:57:13 +0100

    add vector conversion

--- src/units.rs
+++ src/units.rs
@@ -1,2 +1,6 @@
 pub mod energy_units;
 pub mod mass_units;
+
+pub fn convert_data_units(data: &Vec<f64>, conversion: fn(f64) -> f64) -> Vec<f64> {
+    data.iter().map(|x| conversion(*x)).collect()
+}
