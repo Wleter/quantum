0483715701857fb076567dd077a8b42efa58c8fe
Wleter <marcraftzastepczy@gmail.com>
2024-10-04 09:26:32 +0200

    add dirac delta

--- src/states/braket.rs
+++ src/states/braket.rs
@@ -5,8 +5,21 @@ pub struct Braket<T, V> {
     pub bra: StatesElement<T, V>,
 }
 
-#[derive(PartialEq)]
 pub struct StateBraket<T, V> {
     pub ket: (T, V),
     pub bra: (T, V),
+}
+
+impl<T: PartialEq, V: PartialEq> StateBraket<T, V> {
+    pub fn is_diagonal(&self) -> bool {
+        self.ket == self.bra
+    }
+}
+
+pub fn dirac_delta<T: PartialEq, V: PartialEq, const N: usize>(brakets: [StateBraket<T, V>; N]) -> f64 {
+    if brakets.iter().all(|x| x.is_diagonal()) {
+        1.0
+    } else {
+        0.0
+    }
 }
